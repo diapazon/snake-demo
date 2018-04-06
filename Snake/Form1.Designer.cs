92a7d6ea53433d6f50115da8e34d539bb97518f1
diapazon <nurlanro@code.edu.az>
2018-04-06 12:26:05 +0400

    salam

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Snake
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(167, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label1);
+            this.Name = "Snake";
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Snake_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.up_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ilan;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Snake
+{
+    public partial class Snake : Form
+    {
+
+        public Button yem;
+        public Button beden;
+        public Timer timer1;
+        Random rand = new Random();
+        List<Button> snake = new List<Button>();
+        public int TopPos = 0;
+        public int LeftPos = 0;
+       
+        public int counter = 60;
+        
+        public Snake()
+        {
+            InitializeComponent();
+            ilan = new Button();
+            ilan.Text = "yilan";
+            ilan.FlatStyle = FlatStyle.Flat;
+            ilan.BackColor = Color.Brown;
+            ilan.Width = 40;
+            ilan.Height = 40;
+            ilan.Top = TopPos;
+            ilan.Left = LeftPos;
+            ilan.Enabled = false;
+            Controls.Add(ilan);
+            snake.Add(ilan);
+            mealcreate();
+            MealEatMethod();
+              
+     
+        }
+
+       
+
+        private void up_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    snake[i].Top = snake[i].Top - 40;
+                    if (snake[i].Top > 400)
+                    {
+                        snake[i].Top = 0;
+                    }
+                    else if (snake[i].Top < 0)
+                    {
+                        snake[i].Top = 400;
+                    }
+
+                }
+
+
+
+
+
+
+                //if (snake[snake.Count]==snake.[snake.Count])
+                //{
+
+                //}
+                MealEatMethod();
+            //    ExtendTheLenghtUp(); //////////////
+               
+
+
+            }
+          
+            if (e.KeyCode == Keys.Down)
+            {
+         //       TimeCountDownMethod();
+                MealEatMethod();
+                ilan.Top = ilan.Top + 40;
+                if (ilan.Top > 400)
+                {
+                    ilan.Top = 0;
+                }
+                else if (ilan.Top < 0)
+                {
+                    ilan.Top = 400;
+                }
+
+
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+            //    TimeCountDownMethod();
+
+                MealEatMethod();
+                ilan.Left = ilan.Left - 40;
+                if (ilan.Left > 400)
+                {
+                    ilan.Left = 0;
+                }
+                else if (ilan.Left < 0)
+                {
+                    ilan.Left = 400;
+                }
+
+            }
+           
+            if (e.KeyCode == Keys.Right)
+            {
+             //   TimeCountDownMethod();
+
+                MealEatMethod();
+                ilan.Left = ilan.Left + 40;
+                if (ilan.Left > 400)
+                {
+                    ilan.Left = 0;
+                }
+                else if (ilan.Left < 0)
+                {
+                    ilan.Left = 400;
+                }
+
+            }
+           
+        }
+        void mealcreate()
+        {
+
+
+            while (true)
+            {
+                int x = rand.Next(1, 400);
+                
+                if (x % 40 == 0)
+                {
+                yem = new Button();
+                Controls.Add(yem);
+
+                yem.Top = x;
+                    yem.Left = x;
+                    yem.Text = "yem";
+                    yem.Width = 40;
+                    yem.Height = 40;
+                    yem.BackColor = Color.Tomato;
+                    yem.Enabled = false;
+                    break;
+                }
+                
+            }
+            
+        }
+        void MealEatMethod()
+        {
+            if ((ilan.Top == yem.Top) && (ilan.Left == yem.Left))
+            {
+                ilan.BackColor = Color.Green;
+                playSimpleSound();
+                Controls.Remove(yem);
+                ExtendTheLenghtUp();
+                //////////////////////
+                //beden = new Button();
+                //beden.Width = 40;
+                //beden.Height = 40;
+                //Controls.Add(beden);
+                //snake.Add(beden);
+                //beden.Enabled = false;
+                mealcreate();
+            }
+        }
+        void TimeCountDownMethod()
+        {
+
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            label1.Text = counter.ToString();
+
+
+             void timer1_Tick(object sender, EventArgs e)
+            {
+                counter--;
+
+                if ((counter == 3) || (counter == 2) || (counter == 1))
+                {
+                    label1.Font = new Font("Arial", 16);
+                }
+                if (counter == 0)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over");
+
+                }
+               
+                label1.Text = "Time: " + counter.ToString();
+                label1.BackColor = Color.Black;
+                label1.ForeColor = Color.White;
+            }
+        }
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\P106\Desktop\c#\ses.wav");
+            simpleSound.Play();
+        }
+
+
+
+        void ExtendTheLenghtUp()
+        {
+            beden = new Button();
+            beden.Width = 40;
+            beden.Height = 40;
+           
+            beden.Left = snake[snake.Count-1].Left;
+            beden.Top = snake[snake.Count-1].Top+40;
+            Controls.Add(beden);
+            snake.Add(beden);
+            beden.Enabled = false;
+
+        }
+        void extendTheLenghtDown()
+        {
+            beden = new Button();
+            beden.Width = 40;
+            beden.Height = 40;
+
+            beden.Left = snake[snake.Count - 1].Left;
+            beden.Top = snake[snake.Count - 1].Top-40;
+            Controls.Add(beden);
+            snake.Add(beden);
+            beden.Enabled = false;
+        }
+        void extendTheLenghtLeft()
+        {
+            beden = new Button();
+            beden.Width = 40;
+            beden.Height = 40;
+
+            beden.Left = snake[snake.Count - 1].Left + 40;
+            beden.Top = snake[snake.Count - 1].Top;
+            Controls.Add(beden);
+            snake.Add(beden);
+            beden.Enabled = false;
+        } 
+        void extendTheLenghtRight()
+        {
+            beden = new Button();
+            beden.Width = 40;
+            beden.Height = 40;
+
+            beden.Left = snake[snake.Count - 1].Left + 40;
+            beden.Top = snake[snake.Count - 1].Top;
+            Controls.Add(beden);
+            snake.Add(beden);
+            beden.Enabled = false;
+        }
+
+
+
+        private void Snake_Load(object sender, EventArgs e)
+        {
+            TimeCountDownMethod();
+            snake.Add(ilan);
+        }
+    }
+}
